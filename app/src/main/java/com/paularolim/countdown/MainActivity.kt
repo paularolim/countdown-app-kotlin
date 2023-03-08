cb84394b38606d8672b5acce0737803adca6f400
Paula Rolim <rolim.paula1999@gmail.com>
2023-03-07 21:00:22 -0300

    feat: navigate to form screen

--- app/src/main/java/com/paularolim/countdown/MainActivity.kt
+++ app/src/main/java/com/paularolim/countdown/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.paularolim.countdown
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.paularolim.countdown.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnAdd.setOnClickListener {
+            val intent = Intent(this, FormActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
