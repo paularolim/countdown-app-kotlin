9c8edf22b5418d579f75a3cee6f885b98f8ed8c2
Paula Rolim <rolim.paula1999@gmail.com>
2023-03-07 21:25:10 -0300

    feat: add event to database

--- app/src/main/java/com/paularolim/countdown/FormActivity.kt
+++ app/src/main/java/com/paularolim/countdown/FormActivity.kt
@@ -3,10 +3,33 @@ package com.paularolim.countdown
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.paularolim.countdown.databinding.ActivityFormBinding
+import com.paularolim.countdown.models.Event
+import java.util.*
 
-class FormActivity:AppCompatActivity() {
+class FormActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFormBinding
+
+    private fun createEvent() {
+        val db = Firebase.firestore
+
+        val title = binding.editTitle.text.toString()
+        val date = Date(binding.editDate.text.toString()).time
+        val event = Event(title, date)
+
+        db
+            .collection("events")
+            .add(event)
+            .addOnSuccessListener {
+                binding.editTitle.setText("")
+                binding.editDate.setText("")
+                Toast.makeText(this, "Evento salvo com sucesso!", Toast.LENGTH_LONG).show()
+            }.addOnFailureListener {
+                Toast.makeText(this, "Erro ao salvar o evento", Toast.LENGTH_LONG).show()
+            }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,9 +39,7 @@ class FormActivity:AppCompatActivity() {
         setContentView(view)
 
         binding.btnSave.setOnClickListener {
-            binding.editTitle.setText("")
-            binding.editTextDate.setText("")
-            Toast.makeText(this, "Event saved", Toast.LENGTH_LONG).show()
+            createEvent()
         }
     }
 }
