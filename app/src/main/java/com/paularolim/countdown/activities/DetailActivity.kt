9f63a53abdca68af7306c0868e580c752eb54efa
Paula Rolim <rolim.paula1999@gmail.com>
2023-03-09 11:48:50 -0300

    feat: navigate to form on edit mode

--- app/src/main/java/com/paularolim/countdown/activities/DetailActivity.kt
+++ app/src/main/java/com/paularolim/countdown/activities/DetailActivity.kt
@@ -1,7 +1,9 @@
 package com.paularolim.countdown.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.paularolim.countdown.FormActivity
 import com.paularolim.countdown.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
@@ -14,11 +16,26 @@ class DetailActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        val title = intent.getStringExtra("title")
-        val date = intent.getStringExtra("date")
-        val days = intent.getStringExtra("days")
+        val title = intent.getStringExtra("title") ?: ""
+        val date = intent.getStringExtra("date") ?: ""
+        val days = intent.getStringExtra("days") ?: ""
+
+        setData(title, date, days)
+
+        binding.btnDetailEdit.setOnClickListener { navigateToForm(title, date) }
+    }
+
+    private fun setData(title: String, date: String, days: String) {
         binding.txtDetailTitle.text = title
         binding.txtDetailDate.text = date
         binding.txtDetailDays.text = days
+    }
+
+    private fun navigateToForm(title: String, date: String) {
+        val intent = Intent(this, FormActivity::class.java)
+        intent.putExtra("mode", "edit")
+        intent.putExtra("title", title)
+        intent.putExtra("date", date)
+        startActivity(intent)
     }
 }
